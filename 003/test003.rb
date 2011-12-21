6d1359298d9a3ec659f34405d67a7c4c9d6e3364
Paul Kizior <kizior@gmail.com>
2011-12-20 23:02:32 -0600

    attemp 2 - finished lower level methods and got tests passing

--- 003/test003.rb
+++ 003/test003.rb
@@ -3,13 +3,59 @@ require './003'
 
 class Test003 < Test::Unit::TestCase
 
-  def testPrimeFacCount2
+  #-------------------------
+  # Test isPrime
+  #-------------------------
+  def testIsPrime2
+    assert isPrime(2), '2 is a prime number!'
+  end
+
+  def testIsPrime13
+    assert isPrime(13), '13 is a prime number!'
+  end
+  
+  def testIsPrime14
+    assert ! isPrime(14), '14 is not a prime number!'
+  end
+  
+  def testIsPrime101
+    assert isPrime(101), '101 is a prime number!'
+  end
+  
+  def testIsPrime221
+    assert ! isPrime(221), '221 is not a prime number!'
+  end
+
+  #-------------------------
+  # Test getNextPrime
+  #-------------------------
+  def testGetNextPrime2
+    n = getNextPrime 2
+    assert_equal n, 3
+  end
+  
+  def testGetNextPrime13
+    n = getNextPrime 13
+    assert_equal n, 17
+  end
+  
+  def testGetNextPrime64
+    n = getNextPrime 64
+    assert_equal n, 67
+  end
+
+  #-------------------------
+  # Test getPrimeFactors
+  #-------------------------  
+  def testPrimeFactorsCount2
     a = getPrimeFactors 2
+    
     assert_equal 1, a.length
   end
   
-  def testPrimeFacContent2
+  def testPrimeFactorsContent2
     a = getPrimeFactors 2
+    
     if (a.length == 1)
       assert_equal 2, a[0]
     else
@@ -17,12 +63,33 @@ class Test003 < Test::Unit::TestCase
     end
   end
     
-  def testPrimeFacCount10
+  def testPrimeFactorsCount10
+    a = getPrimeFactors 10
 
-    a = getPrimeFactors 2
-
-    assert_equal 3, a.length
-
+    assert_equal 2, a.length
   end
+  
+  def testPrimeFacContentLowestIsFirst
+    a = getPrimeFactors 10
+    
+    if (a.length == 2)
+      assert_equal 2, a[0]
+    else
+      assert false, 'Did not have the correct number of factors.'
+    end
+  end
+  
+  def testPrimeFacContent10
+    a = getPrimeFactors 10
+
+    assert_equal [2, 5], a
+  end
+  
+  def testPrimeFacContent284
+    a = getPrimeFactors 284
+    
+    assert_equal [2, 2, 71], a
+  end
+  
 
 end
