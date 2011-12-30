ffab4d9490bc89fc438023dacac87eb416c25d09
Paul Kizior <kizior@gmail.com>
2011-12-29 23:18:41 -0600

    forgot to commit function definition in refactored problem five

--- 005/lib005.rb
+++ 005/lib005.rb
@@ -1,30 +1,34 @@
 require '../003/lib003'
 
-factorColl = {}
+def getLeastGreatestMultiple coll
 
-for i in 2..20
+  factorColl = {}
 
-  factors = getPrimeFactors(i)
-  
-  fPrime = {}
-  for f in factors
-    if ! fPrime.include?(f)
-      fPrime[f] = 0
+  for i in coll
+
+    factors = getPrimeFactors(i)
+    
+    fPrime = {}
+    for f in factors
+      if ! fPrime.include?(f)
+        fPrime[f] = 0
+      end
+      fPrime[f] += 1
+    end  
+    
+    for k in fPrime.each_key
+      if (! factorColl.include?(k)) || factorColl[k] < fPrime[k]
+        factorColl[k] = fPrime[k]
+      end
     end
-    fPrime[f] += 1
-  end  
-  
-  for k in fPrime.each_key
-    if (! factorColl.include?(k)) || factorColl[k] < fPrime[k]
-      factorColl[k] = fPrime[k]
-    end
+
   end
 
-end
+  result = 1
+  for k in factorColl.each_key
+    result = result * (k ** factorColl[k])
+  end
 
-result = 1
-for k in factorColl.each_key
-  result = result * (k ** factorColl[k])
-end
+  return result
 
-puts result
+end
--- 005/test005.rb
+++ 005/test005.rb
@@ -3,6 +3,14 @@ require './lib005'
 
 class Test005 < Test::Unit::TestCase
 
+  def testGetLeastGreatestMultiple2And3
+    assert_equal 6, getLeastGreatestMultiple([2,3])
+  end
+  
+  def testGetLeastGreatestMultiple6And10
+    assert_equal 30, getLeastGreatestMultiple([6,10])
+  end
+
   def testGetLeastGreatestMultiple1to10
     assert_equal 2520, getLeastGreatestMultiple(2..10)
   end
